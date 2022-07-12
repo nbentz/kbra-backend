e8d5d25d2ed7ab4daf9b85b9c3095253b4116f6a
Nicholas Bentzen <bentzen.nich@gmail.com>
2022-07-12 13:06:14 -0500

    removed unique for phone, email fields in Contact model.

--- kbra/models/contact.py
+++ kbra/models/contact.py
@@ -6,6 +6,6 @@ class Contact(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, blank=True, editable=False)
     first_name = models.CharField(max_length=255, default='')
     last_name = models.CharField(max_length=255, default='')
-    phone = models.CharField(max_length=50, unique=True, default=None, null=True)
-    email = models.CharField(max_length=255, unique=True, default=None, null=True)
+    phone = models.CharField(max_length=50, default=None, null=True)
+    email = models.CharField(max_length=255, default=None, null=True)
     address = models.TextField(default=None, null=True)
